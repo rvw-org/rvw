c224e5c9490fab902e35d22705f897f9c6df134e
Ivan Pavlov <pavlov.ivan095@gmail.com>
2018-06-06 00:46:40 +0200

    cherrypick

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,9 +1,15 @@
 library(testthat)
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 library(rvw)
 
 test_check("rvw")
+=======
+library(rvwgsoc)
+
+test_check("rvwgsoc")
+>>>>>>> 9915602... Fix error in optimisation parameters with different learning algorithms. Add protection from possible bugs in nn parameter. Set up package testing.
 =======
 library(rvwgsoc)
 
