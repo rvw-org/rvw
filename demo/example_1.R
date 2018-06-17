ad81208b98c05f873f54fdd33ce65fbc922a82d7
Ivan Pavlov <pavlov.ivan095@gmail.com>
2018-06-18 01:45:58 +0200

    cherrypick

---
+++ demo/example_1.R
@@ -0,0 +1,17 @@
+library(rvwgsoc)
+
+
+curr_dir <- getwd()
+setwd(tempdir())
+
+# Right now using only files in .vw format, later will also accept R formats
+ext_train_data <- system.file("extdata", "X_train.vw", package = "rvwgsoc")
+ext_test_data <- system.file("extdata", "X_valid.vw", package = "rvwgsoc")
+
+test_vwmodel <-  vwsetup(dir = "./", model = "mdl.vw",
+                         general_params = list(hash="all"),
+                         optimization_params = list(adaptive=FALSE, learning_rate=0.1))
+vwtrain(test_vwmodel, data_path = ext_train_data)
+vw_output <- vwtest(test_vwmodel, data_path = ext_test_data, probs_path = "./probs.vw")
+
+setwd(curr_dir)
--- tests/testthat/test-vwsetup.R
+++ tests/testthat/test-vwsetup.R
@@ -3,10 +3,10 @@ library(rvwgsoc)
 
 test_model <- list(params = list(learning_mode = "binary",
                                  algorithm = "sgd",
-                                 general_params = list(cache = FALSE,
+                                 general_params = list(cache=FALSE,
                                                        passes=1,
                                                        bit_precision=18,
-                                                       qudratic=FALSE,
+                                                       quadratic=FALSE,
                                                        cubic=FALSE,
                                                        interactions="",
                                                        permutations=FALSE,
@@ -26,7 +26,7 @@ test_model <- list(params = list(learning_mode = "binary",
                                    adaptive=TRUE,
                                    normalized=TRUE,
                                    invariant=TRUE,
-                                   hessian=FALSE,
+                                   hessian_on=FALSE,
                                    initial_pass_length="",
                                    l1=0,
                                    l2=0,
@@ -36,12 +36,16 @@ test_model <- list(params = list(learning_mode = "binary",
                                    learning_rate=0.5,
                                    loss_function="squared",
                                    quantile_tau=0.5)
+
 ),
 dir = "../my_tmp/",
 model = "mdl.vw",
-params_str = paste("  --passes 1 --bit_precision 18 --holdout_period 10 --early_terminate 3 ",
-                   "--initial_weight 0 --adaptive --normalized --invariant --l1 0 --l2 0 --decay_learning_rate 1",
-                   " --initial_t 0 --power_t 0.5 --learning_rate 0.5 --loss_function squared --quantile_tau 0.5", sep = ""),
+params_str = paste("  --passes 1 --bit_precision 18 --holdout_period 10",
+                   " --early_terminate 3 --initial_weight 0 --adaptive",
+                   " --normalized --invariant --l1 0 --l2 0",
+                   " --decay_learning_rate 1 --initial_t 0 --power_t 0.5",
+                   " --learning_rate 0.5 --loss_function squared",
+                   " --quantile_tau 0.5", sep = ""),
 data = list(train = "",
             test = ""),
 cache = list(train = "",
@@ -50,29 +54,73 @@ eval = "")
 class(test_model) <- "vw"
 
 test_that("vwsetup correctly setup model with different learning modes", {
+  # Empty setup
   expect_identical(vwsetup(dir = "../my_tmp/"), test_model)
-  expect_identical(vwsetup(dir = "../my_tmp/", learning_mode = "nn"), {test_model$params$learning_mode = "nn";
-  test_model$params$learning_params = list(hidden=3,
+  
+  # Reference test model for nn mode
+  nn_test_model <- test_model
+  nn_test_model$params$learning_mode = "nn"
+  nn_test_model$params$learning_params = list(hidden=3,
                                            inpass=FALSE,
                                            multitask=FALSE,
                                            dropout=FALSE,
-                                           meanfield=FALSE);
-  test_model$params_str = paste("--nn 3  --passes 1 --bit_precision 18 --holdout_period 10 --early_terminate 3 ",
-                                "--initial_weight 0 --adaptive --normalized --invariant --l1 0 --l2 0 --decay_learning_rate 1",
-                                " --initial_t 0 --power_t 0.5 --learning_rate 0.5 --loss_function squared --quantile_tau 0.5", sep = "");
-  test_model})
-  expect_identical(vwsetup(dir = "../my_tmp/", learning_mode = "lda"), {test_model$params$learning_mode = "lda";
-  test_model$params$learning_params = list(num_topics=0,
+                                           meanfield=FALSE)
+  nn_test_model$params_str = paste("--nn 3  --passes 1 --bit_precision 18 --holdout_period 10",
+                                " --early_terminate 3 --initial_weight 0 --adaptive",
+                                " --normalized --invariant --l1 0 --l2 0",
+                                " --decay_learning_rate 1 --initial_t 0 --power_t 0.5",
+                                " --learning_rate 0.5 --loss_function squared",
+                                " --quantile_tau 0.5", sep = "")
+  expect_identical(
+    vwsetup(dir = "../my_tmp/", learning_mode = "nn"),
+    nn_test_model
+  )
+  
+  # Reference test model for lda mode
+  lda_test_model <- test_model
+  lda_test_model$params$learning_mode = "lda"
+  lda_test_model$params$learning_params = list(num_topics=0,
                                            lda_alpha=0.100000001,
                                            lda_rho=0.100000001,
                                            lda_D=10000,
                                            lda_epsilon=0.00100000005,
                                            math_mode=0,
-                                           minibatch=1);
-  test_model$params_str = paste("--lda 0  --passes 1 --bit_precision 18 --holdout_period 10 --early_terminate 3 ",
-                                "--initial_weight 0 --lda_alpha 0.100000001 --lda_rho 0.100000001 --lda_D 10000",
-                                " --lda_epsilon 0.00100000005 --math_mode 0 --minibatch 1 --adaptive --normalized",
-                                " --invariant --l1 0 --l2 0 --decay_learning_rate 1 --initial_t 0 --power_t 0.5",
-                                " --learning_rate 0.5 --loss_function squared --quantile_tau 0.5", sep = "");
-  test_model})
+                                           minibatch=1)
+  lda_test_model$params_str = paste("--lda 0  --passes 1 --bit_precision 18",
+                                " --holdout_period 10 --early_terminate 3",
+                                " --initial_weight 0 --lda_alpha 0.100000001",
+                                " --lda_rho 0.100000001 --lda_D 10000",
+                                " --lda_epsilon 0.00100000005 --math_mode 0",
+                                " --minibatch 1 --adaptive --normalized --invariant",
+                                " --l1 0 --l2 0 --decay_learning_rate 1 --initial_t 0",
+                                " --power_t 0.5 --learning_rate 0.5 --loss_function squared",
+                                " --quantile_tau 0.5", sep = "")
+  
+  expect_identical(
+    vwsetup(dir = "../my_tmp/", learning_mode = "lda"),
+    lda_test_model
+  )
+  
+  # Reference test model with custom parameters
+  custom_test_model <- test_model
+  custom_test_model$params$general_params$cache = TRUE
+  custom_test_model$params$general_params$passes = 10
+  custom_test_model$params$optimization_params$adaptive = FALSE
+  custom_test_model$params$learning_params$binary = TRUE
+  custom_test_model$params_str <- paste("  --cache --passes 10 --bit_precision 18",
+                                 " --holdout_period 10 --early_terminate 3",
+                                 " --initial_weight 0 --binary --normalized",
+                                 " --invariant --l1 0 --l2 0 --decay_learning_rate 1",
+                                 " --initial_t 0 --power_t 0.5 --learning_rate 0.5",
+                                 " --loss_function squared --quantile_tau 0.5", sep = "")
+  # Package vwmodel setup
+  test_vwmodel <- vwsetup(
+    dir = "../my_tmp/",
+    cache = TRUE,
+    general_params = list(passes=10),
+    optimization_params = list(adaptive=FALSE),
+    learning_params = list(binary=TRUE)
+  )
+  expect_identical(test_vwmodel, custom_test_model)
+  
 })
