77b77684ba610640dc3d99d8ff1f6b47737c32d9
James J Balamuta <balamut2@illinois.edu>
2018-06-19 10:31:01 -0500

    Remove old R files

--- tests/testthat/test_dt2vw.R
+++
@@ -1,8 +0,0 @@
-library(rvw)
-context("basic tests for dt2vw")
-
-test_that("dt2vw raises error when NAs in input dataset", {
-  df <- data.frame(v1 = c(1,2,NA,4), v2 = letters[1:4], target = c(0,0,1,1))
-  expect_error(rvw::dt2vw(data = df, fileName = "num_NA.vw", target = "target"),
-               regexp = "Please remove any 'NA' values from data first.")
-})
